a3c7e687c1ec600d82a0297d58d2d5a1c3bda7a4
Tyler Harp <tharp@andrew.cmu.edu>
2025-07-18 17:58:06 -0400

    adding keyword keep for log utils

--- log_utils/log_editing.py
+++ log_utils/log_editing.py
@@ -19,21 +19,35 @@ def remove_lines_by_keywords(lines, keywords):
     """
     filtered_lines = []
     for line in lines:
-        if not any(keyword in line for keyword in keywords):
+        if not any(keyword in line.lower() for keyword in keywords):
             filtered_lines.append(line)
     return filtered_lines
 
+def keep_lines_by_keywords(lines, keywords):
+    """
+    Keep only lines from a list that contain any of the specified keywords.
+    """
+    kept_lines = []
+    for line in lines:
+        if any(keyword in line.lower() for keyword in keywords):
+            kept_lines.append(line)
+    return kept_lines
+
 if __name__ == "__main__":
     # Example usage
-    input_file_path = "/home/tyler/Downloads/16-58-rgb-stream-not-playable.log"
+    input_file_path = "/home/tyler/Downloads/docker.log"
     output_file_path = "/home/tyler/Downloads/filtered_log.txt"
-    keywords_to_remove = ["foxglove_bridge", "mavros_node", "doodle_companion_publisher"]
 
     # Load the file
     lines = load_txt_file(input_file_path)
 
     # Remove lines containing the specified keywords
+    keywords_to_remove = ["foxglove_bridge", "mavros_node", "doodle_companion_publisher", "transport", "zenoh"]
     filtered_lines = remove_lines_by_keywords(lines, keywords_to_remove)
+
+    # Keep lines containing the specified keywords
+    # keywords_to_keep = ["rtsp_detection", "doodle_companion_publisher"]
+    # filtered_lines = keep_lines_by_keywords(lines, keywords_to_keep)
 
     # Save the filtered lines to a new file
     save_txt_file(output_file_path, filtered_lines)
